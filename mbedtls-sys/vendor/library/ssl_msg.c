3ba1886381223267858afc6169a6b1a4135f45a7
Bradley Landherr <12598313+landhb@users.noreply.github.com>
2021-06-08 12:22:38 -0400

    Source patch

--- mbedtls-sys/vendor/library/ssl_msg.c
+++ mbedtls-sys/vendor/library/ssl_msg.c
@@ -1168,7 +1168,8 @@ static void mbedtls_ssl_cf_memcpy_if_eq( unsigned char *dst,
     const unsigned char mask = (unsigned char) mbedtls_ssl_cf_mask_from_bit( equal );
 
     /* dst[i] = c1 == c2 ? src[i] : dst[i] */
-    for( size_t i = 0; i < len; i++ )
+    size_t i = 0;
+    for( i = 0; i < len; i++ )
         dst[i] = ( src[i] & mask ) | ( dst[i] & ~mask );
 }
 
