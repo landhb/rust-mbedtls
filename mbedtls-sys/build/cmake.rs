503735b7b23285d1dcc1b75c66669b6334668d57
Bradley Landherr <12598313+landhb@users.noreply.github.com>
2021-09-27 12:52:10 -0400

    Update cmake.rs

--- mbedtls-sys/build/cmake.rs
+++ mbedtls-sys/build/cmake.rs
@@ -17,6 +17,7 @@ impl super::BuildConfig {
         ))
         .define("ENABLE_PROGRAMS", "OFF")
         .define("ENABLE_TESTING", "OFF")
+        .define("CMAKE_BUILD_TYPE", "Release")
         .build_target("lib");
         for cflag in &self.cflags {
             cmk.cflag(cflag);
